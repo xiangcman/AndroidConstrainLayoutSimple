1f2a951d29f352f57c29498e77f64d94c1c5c64a
xiangcheng <a1002326270@163.com>
2018-12-26 14:10:59 +0800

    first commit

---
+++ app/src/main/java/com/single/constrainlayout/Simple2Activity.java
@@ -0,0 +1,13 @@
+package com.single.constrainlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Simple2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple1);
+    }
+}
